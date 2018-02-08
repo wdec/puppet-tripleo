be5b3af4cb1ea5cf39bfdf7e95b76b4f1a3e188b
Wojciech Dec <wdec@cisco.com>
2018-02-08 15:49:26 +0100

    Initial collectd changes
    Change-Id: Ifb582b96253908e7480c96e3c03dd96dd14fadce
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

---
+++ manifests/profile/base/collectd.pp
@@ -0,0 +1,76 @@
+# Copyright 2018 Cisco, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::collectd
+#
+# Collectd profile for TripleO
+#
+# === Parameters
+#
+#
+# [*plugin_configs*]
+#   Keyed hash of configs, with key being the plugin name.
+#   For each plugin a "content" key MUST be present, which can be empty.
+#   Defaults to hiera('tripleo::collectd:plugin_config')
+#
+#
+# [*purge*]
+#   Purge default configuration and plugin configurations
+#   Defaults to true
+#
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+class tripleo::profile::base::collectd (
+  $configs      = hiera('tripleo::collectd:plugin_configs', undef),
+  $purge        = hiera('tripleo::collectd:purge', true),
+  $step         = hiera('step'),
+)
+  {
+    if $purge {
+      class { '::collectd':
+        purge           => true,
+        recurse         => true,
+        purge_config    => true,
+      }
+    }
+
+    # Get the configs for earch plugin
+    # The code is a basic key iteraton routine workaround due to puppet 3 not having an iterator.
+    $plugin_names = keys($configs)
+
+    if !empty($plugin_names) {
+        tripleo::collectd::plugins { $plugin_names:
+        configs => $configs
+      }
+    }
+    else {
+      include ::collectd
+    }
+
+    define tripleo::collectd::plugins ($configs) {
+      # Name will take the value of each entry for the plugin_names
+      $config = $configs[$name]
+
+      collectd::plugin { $name:
+        content => join($config["content"], "\n"),
+      }
+    }
+  }
+
+
+
